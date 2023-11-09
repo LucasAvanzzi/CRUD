bd7d104a8a02953ca4f3a84226b59e41d157d74b
desig <desig@LAPTOP-QOQ5A7IP>
2023-11-09 15:40:03 -0300

    Adicionar arquivos de projeto.

---
+++ BancoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Data
+{
+	public class BancoContext : DbContext
+	{
+		public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+		{ 
+		}
+
+		public DbSet<ContatoModel> Contatos { get; set; }
+	}
+}
---
+++ CRUD/Controllers/ContatoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
